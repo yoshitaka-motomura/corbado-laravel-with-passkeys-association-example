949a939a2ef6769cee798d0c78c1ec02e444062c
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-05-22 02:05:03 +0900

    ⚡ laravel breeze installed

---
+++ service/resources/js/Pages/Hello.tsx
@@ -0,0 +1,13 @@
+import { Head } from "@inertiajs/react";
+import React from "react";
+
+export default function Hello() {
+    return (
+        <div>
+            <Head>
+                <title>Hello</title>
+            </Head>
+            <h1>Hellodesu</h1>
+        </div>
+    );
+}
